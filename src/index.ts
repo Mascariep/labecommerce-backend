e3e6d941a0aadb6ebe2636202fa44eb543024442
Mascariep <edipohc@gmail.com>
2023-01-10 15:32:00 -0300

    continuação do express e postman

--- src/index.ts
+++ src/index.ts
@@ -174,3 +174,102 @@ app.get("/users", (req: Request, res: Response)=>{
         purchases.push(newPurchase)
         res.status(201).send("Compra realizada com sucesso")
         })
+
+//=================================================================================
+
+// 1) validar um produto por id
+app.get("/products/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const result = products.find((products) => products.id === id)
+    res.status(200).send(result)
+
+})
+
+//validar aos itens por usuario id
+app.get("/users/:id/purchases", (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const result = purchases.filter((purchase) => purchase.userId === id)
+    res.status(200).send(result)
+})
+
+//=================================================================================
+// 2) deletar um item do array
+
+//======delete de usuario======
+app.delete("/users/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+
+    //encontrar o indice do item a ser removido
+    const indexToRemove = users.findIndex((user) => user.id === id)
+    
+    //só deleter caso o indice seja válido (ou seja, encontrou o item)
+    if (indexToRemove >= 0){
+        users.splice(indexToRemove, 1)
+    }
+
+    res.status(200).send("User apagado com sucesso")
+})
+
+//======delete de produto=======
+app.delete("/products/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+
+    //encontrar o indice do item a ser removido
+    const indexToRemove = products.findIndex((product) => product.id === id)
+    
+    //só deleter caso o indice seja válido (ou seja, encontrou o item)
+    if (indexToRemove >= 0){
+        products.splice(indexToRemove, 1)
+    }
+
+    res.status(200).send("Produto apagado com sucesso")
+})
+
+//===================================================================================
+
+// 3) editando um item
+
+//======editando um user======
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const newId = req.body.id as string | undefined
+    const newEmail = req.body.email as string | undefined
+    const newPassword = req.body.password as string | undefined
+
+    const user = users.find((user) => user.id === id)
+
+    if (user) {
+        user.id = newId || user.id
+        user.email = newEmail || user.email
+        user.password = newPassword || user.password
+    }
+
+    res.status(200).send("Cadastro atualizado com sucesso")
+})
+
+//======editando um produto======
+
+app.put("/products/:id", (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const newId = req.body.id as string | undefined
+    const newName = req.body.name as string | undefined
+    const newPrice = req.body.price as number | undefined
+    const newCategory = req.body.category as ProductCategory | undefined
+
+    const product = products.find((product) => product.id === id)
+
+    if (product) {
+        product.id = newId || product.id
+        product.name = newName || product.name
+        product.price = isNaN(newPrice) ? product.price : newPrice
+        product.category = newCategory || product.category
+
+    }
+
+    res.status(200).send("Produto atualizado com sucesso")
+})
